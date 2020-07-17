4f749baacc05bf3b6fc3313c8d0efa1d43b7e9df
gutza <digutza.2@gmail.com>
2020-07-17 11:36:12 -0500

    Adding fetch trending

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,18 @@
 //@flow
 export const FETCH_GIFS = 'FETCH_GIFS';
 
-const GIPHY_API = 'http://api.giphy.com/v1/gifs/search?q=';
-const API_KEY = '&api_key=o5itTJKYKT1U5XwoRLYJVYYidofqLPsU';
+const GIPHY_API = 'http://api.giphy.com/v1/gifs/';
+const API_KEY = 'api_key=o5itTJKYKT1U5XwoRLYJVYYidofqLPsU';
 
 export function fetchGifs(query: string = '') {
-	const URL = `${GIPHY_API}${query.replace(/\s/g, '+')}${API_KEY}`;
+	let URL;
+	const URL_TRENDY = `${GIPHY_API}trending?${API_KEY}`;
+	const URL_SEARCH = `${GIPHY_API}search?q=${query.replace(
+		/\s/g,
+		'+'
+	)}&${API_KEY}`;
+	URL = query === '' ? URL_TRENDY : URL_SEARCH;
+
 	console.log(URL);
 	return (dispatch) => {
 		fetch(URL)
@@ -13,6 +20,7 @@ export function fetchGifs(query: string = '') {
 				return response.json();
 			})
 			.then((body) => {
+				console.log(body);
 				dispatch(gifsFetched(body.data));
 			})
 			.catch((err) => console.log(err));
