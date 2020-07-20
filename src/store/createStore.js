8f18fe99ab30a72173de4ec27a1c824523b8b64c
gutza <digutza.2@gmail.com>
2020-07-20 12:44:59 -0500

    fix explorer

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,15 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
 
 const middlewares = [thunk];
+const composeSetup =
+	process.env.NODE_ENV !== 'production' &&
+	typeof window === 'object' &&
+	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: compose;
 
 let store = createStore(
 	rootReducer,
-	compose(
-		applyMiddleware(...middlewares),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	compose(applyMiddleware(...middlewares), composeSetup)
 );
 
 export default store;
